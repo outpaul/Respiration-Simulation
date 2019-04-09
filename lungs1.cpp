b0812fd1975bb4e72f5f0af4029a22da7724c594
outpaul <bitanpaul8@gmail.com>
2019-04-09 20:04:20 +0530

    Glamor

--- lungs1.cpp
+++ lungs1.cpp
@@ -9,11 +9,14 @@ using namespace std;
 #define HEIGHT 1440
 
 int img[HEIGHT][WIDTH][3] = {0};
-int cap = 2;
+int cap = 0;
 GLdouble upVector[3] = {0, 1.0, 0};
 int angle = 90;
 GLfloat theta = 3.14;
 int displayFlag = 0;
+GLfloat alpha = 0;
+int arrowFlag = 1;
+int scaleFlag = 0;
 
 void init() {
     glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -72,13 +75,14 @@ void drawOxyArrow(GLfloat alpha) {
 
 	glColor4f(1.0, 0.0, 0.0, alpha);
 	glPointSize(4.0);
-	glBegin(GL_LINES);
-	glVertex2i(492, 620);
-	glVertex2i(242, 620);
-	glVertex2i(300, 700);
-	glVertex2i(242, 620);
-	glVertex2i(300, 540);
-	glVertex2i(242, 620);
+    glBegin(GL_POLYGON);
+    glVertex3i(-235, 40, 0);
+	glVertex3i(-235, 110, 0);
+	glVertex3i(-265, 110, 0);
+	glVertex3i(-265, 40, 0);
+	glVertex3i(-280, 40, 0);
+	glVertex3i(-250, 10, 0);
+    glVertex3i(-220, 40, 0);
 	glEnd();
 	glFlush();
 }
@@ -86,18 +90,28 @@ void drawOxyArrow(GLfloat alpha) {
 void drawCarboxyArrow(GLfloat alpha) {
 	glColor4f(0.0, 0.0, 1.0, alpha);
 	glPointSize(4.0);
-	glBegin(GL_LINES);
-	glVertex2i(512, 600);
-	glVertex2i(512, 350);
-	glVertex2i(592, 542);
-	glVertex2i(512, 600);
-	glVertex2i(432, 542);
-	glVertex2i(512, 600);
+	glBegin(GL_POLYGON);
+    glVertex3i(235, 80, 0);
+	glVertex3i(235, 10, 0);
+	glVertex3i(265, 10, 0);
+	glVertex3i(265, 80, 0);
+	glVertex3i(280, 80, 0);
+	glVertex3i(250, 110, 0);
+    glVertex3i(220, 80, 0);
 	glEnd();
 	glFlush();
 }
 
+void drawString(GLfloat x, GLfloat y, GLfloat z, unsigned char str[], int len) {
+        int w = glutBitmapLength(GLUT_BITMAP_8_BY_13, str);
+        glRasterPos3f(x, y, z);
+        for(int i = 0;i < len; i++) {
+            glutBitmapCharacter(GLUT_BITMAP_8_BY_13, str[i]);
+        }
+}
+
 void renderLungs() {
+    glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POINTS);
     for(int i = 0;i < HEIGHT;i++) {
         for(int j = 0;j < WIDTH;j++) {
@@ -108,16 +122,21 @@ void renderLungs() {
     }
 	glEnd();
     glFlush();
+
+    glColor3f(0.0,0.0,0.0);
+    glTranslatef(50, 1300, 0);
+    unsigned char str[] = "Lungs";
+    drawString(0, 0, 0, str, 5);
+    glTranslatef(-50, -1300, 0);
 }
 
 void renderAlveoli (){
     //glClear(GL_COLOR_BUFFER_BIT);
     //lumos();
-	glColor3f(0.7,0.8,0.4);
+    glClearColor(0.2,0.2,0.3, 1.0);
+	glColor3f(0.8,0.6,0.2);
     //-------------ALVEOLI------------------------
     glTranslatef(-100, 50, 0);
-
-
 	glutSolidSphere(100, 100, 100);
     glTranslatef(100, -50, 0);
 
@@ -128,6 +147,13 @@ void renderAlveoli (){
     glTranslatef(0, -50, 0);
     glutSolidSphere(100, 100, 100);
     glTranslatef(0, 50, 0);
+
+    glTranslatef(-10, 300, 0);
+    //glScalef(4.0, 4.0, 4.0);
+    unsigned char str[] = "Alveoli";
+    drawString(0, 0, 0, str, 7);
+    //glScalef(0.25, 0.25, 0.25);
+    glTranslatef(10, -300, 0);
     //-------------------------------------------
 	glColor3f(1.0,0.0,0.0);
 
@@ -142,6 +168,16 @@ void renderAlveoli (){
         glTranslatef(-a, -b, 0.0);
 
     }
+    glColor3f(0.3,0.8,0.6);
+    glTranslatef(-450, 10, 0);
+    unsigned char str1[] = "Deoxygenated Blood";
+    drawString(0, 0, 0, str1, 18);
+    glTranslatef(450, -10, 0);
+
+    glTranslatef(310, 10, 0);
+    unsigned char str2[] = "Oxygenated Blood";
+    drawString(0, 0, 0, str2, 16);
+    glTranslatef(-310, -10, 0);
 
 	glutSwapBuffers();
 }
@@ -150,8 +186,7 @@ void display() {
 
 	//glClear(GL_COLOR_BUFFER_BIT);
 	if(cap == 0) {
-        if(displayFlag)
-            glTranslatef(-512,-720,0.0);
+        init();
         displayFlag = 0;
 		renderLungs();
     }
@@ -230,19 +265,17 @@ void animateAlveoli() {
     float radius = 250;
 	GLfloat step = 1/radius;
     GLfloat a, b;
-    int arrowFlag = 1;
-    GLfloat alpha = 0;
     if(theta > 6.28)
         theta = 3.14;
     //for(GLfloat theta = 3.14; theta <= 6.28; theta += step) {
     	glClear(GL_COLOR_BUFFER_BIT);
-    	/*alpha += ((8 * step * arrowFlag)/(3.14));
-    	if(alpha >= 1)
+        if(alpha >= 1)
     		arrowFlag = -1;
     	else if(alpha <= 0)
     		arrowFlag = 1;
+    	alpha += ((8 * step * arrowFlag)/(3.14));
     	drawOxyArrow(alpha);
-    	drawCarboxyArrow(1 - alpha);*/
+    	drawCarboxyArrow(1 - alpha);
     	for(int i = 0;i < 6;i++) {
     		int flag = 0;
     		a = (radius * cos(theta + i * 1.047));
@@ -272,14 +305,36 @@ void animate() {
 	else
 		animateAlveoli();
 }
-void start(int button, int state, int x, int y) {
+void start(int button, int state, int x2, int y2) {
+
+    GLfloat x1 = x2 - 512;
+    GLfloat y1 = y2 - 720;
 
     if(button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-        x = 0;
-        y = 0;
-        xspeed = 1;
-        yspeed = 1;
-        cap = (cap + 2)%4;
+        if(cap == 0) {
+            cap = 2;
+            return;
+        }
+        glTranslatef(x1, y1, 0.0);
+        glScalef(1.3, 1.3, 1.3);
+        glTranslatef(-x1, -y1, 0.0);
+        //scaleFlag++;
+    }
+    if(button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
+        if(cap == 0)
+            return;
+        glTranslatef(x1, y1, 0.0);
+        glScalef(1/1.3, 1/1.3, 1/1.3);
+        glTranslatef(-x1, -y1, 0.0);
+        //scaleFlag--;
+    }
+}
+
+void reset(unsigned char key, int x, int y) {
+    if(key == 'r') {
+        init();
+        if(displayFlag)
+            glTranslatef(512, 720, 0.0);
     }
 }
 
@@ -329,6 +384,7 @@ int main(int argc, char **argv) {
     glutIdleFunc(animate);
     glutMouseFunc(start);
     glutSpecialFunc(handleKey);
+    glutKeyboardFunc(reset);
     glutMainLoop();
 
 
